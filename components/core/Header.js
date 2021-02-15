eae413507b4ddf1f61f7a9cf3248b31dc36c1834
gentlysharp <sanghyuphan33@gmail.com>
2021-02-15 23:56:11 +0900

    home 모바일 크기 작업

--- components/core/Header.js
+++ components/core/Header.js
@@ -4,6 +4,9 @@ import {useState} from 'react'
 import styles from '../../styles/Layout.module.css'
 
 export default function Header() {
+
+  const [toggle, setToggle ] = useState(false);
+
   const [menus,setMenus] = useState([
       { title:"ROSTER", path:"/roster", subMenus:[] },
       { title:"STADIUM", path:"/stadium", subMenus:[] },
@@ -14,10 +17,61 @@ export default function Header() {
       { title:"SHOP", path:"/shop", subMenus:[] }
     ]
   )
-  
+
+  const [icons, setIcons] = useState([
+    { href:"#", name:"fab fa-instagram" },
+    { href:"#", name:"fab fa-facebook-f" },
+    { href:"#", name:"", src:"/images/naver_blog.png" },
+    { href:"#", name:"fab fa-instagram" },
+  ])
+
+  const toggleMenu = () => {
+    setToggle(!toggle)
+  }
 
   return (
     <header className={styles.header}>
+      <div className={styles.header__btn} onClick={() => toggleMenu()}>
+        { toggle 
+          ? <div>
+              <i className="fas fa-bars"></i>
+            </div>
+          : <div>
+              <i className="fas fa-times"></i>
+            </div>
+        }
+      </div>
+
+      { !toggle
+         &&  <div className={styles.header__dropdown}>
+              <ul>
+                {menus.map((menu,i) => 
+                  <li key={"menu" + i} className={styles.header__dropdown_menu}>
+                    <Link href={menu.path}>{menu.title}</Link>
+                  </li>
+                )}
+              </ul>
+      
+              <div className={styles.header__dropdown__footer}> 
+                {
+                  icons.map((icon,i) => 
+                    <a key={"icon" + i}>
+                      { icon.name !== "" 
+                        ? <button>
+                          <i className={icon.name}></i>
+                        </button>
+                        : <button>
+                          <img className={styles.header__upper_nav_img} src={icon.src}></img>
+                        </button>
+                      }
+                    </a>
+                  )
+                }
+              </div>
+            </div>
+      }
+     
+
       <div className={styles.header__logo_wrap}>
         <Link href="/">
           <a href="/">
@@ -29,41 +83,21 @@ export default function Header() {
     <div className={styles.header__upper_nav}>
       <div className={styles.header__upper_nav_inner}>
         <ul>
-          <li>
-            <a href="#">
-              <button>
-                <i className="fab fa-instagram"></i>
-              </button>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <button>
-                <i className="fab fa-facebook-f"></i>
-              </button>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <button>
-                <img className={styles.header__upper_nav_img} src="/images/naver_blog.png"></img>
-              </button>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <button>
-                <i className="fab fa-facebook-f"></i>
-              </button>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <button>
-                <i className="fab fa-facebook-f"></i>
-              </button>
-            </a>
-          </li>
+          { icons.map((icon,i) => 
+            <li key={"icon" + i}>
+              <a>
+                { icon.name !== "" 
+                  ? <button>
+                    <i className={icon.name}></i>
+                  </button>
+                  : <button>
+                    <img className={styles.header__upper_nav_img} src={icon.src}></img>
+                  </button>
+                }
+              </a>
+            </li>
+            )
+          }
         </ul>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -28,45 +28,58 @@ export default function Home() {
         <img className="w-full" style={{maxWidth:"1440px"}} src="./images/home-img-00.jpg"></img>
       </div>
       <HeroSection >
-        <div className="flex flex-row-reverse justify-center">
-          <div className="m-10" style={{width:'400px'}}>
+        <div className={styles.home__card_reverse}>
+          <div className={styles.hidden_btn}>
+            <button className={styles.text_btn}>
+              <span className="m-2">
+                <i class="fas fa-chevron-right"></i>
+              </span>
+              MORE
+            </button>
+          </div>
+          <div className={styles.home__card_img}>
             <img src="./images/home-img-01.jpg"></img>
           </div>
 
-          <div className="flex flex-col m-10 mx-20">
+          <div className={styles.home__card_content}>
             <div className={styles.content_title}>
-                <p className={styles.sub_title}>고양불스 선수와 함께하는</p>
-                <p>YOUTH <strong style={{color:'#fff'}}>ACADEMY</strong></p>
-              </div>
-              <div className={styles.academy}>
-                <p>
-                  <button className={styles.text_btn}>U-4</button>
-                  <button className={styles.text_btn}>U-5</button>
-                  <button className={styles.text_btn}>U-6</button>
-                </p>
-                <p>
-                  <button className={styles.text_btn}>U-10</button>
-                  <button className={styles.text_btn}>U-12</button>
-                </p>
+              <p className={styles.sub_title}>고양불스 선수와 함께하는</p>
+              <p>YOUTH <strong style={{color:'#ffe500'}}>ACADEMY</strong></p>
+            </div>
+            <div className={styles.text_btn_group }>
+              <p>
+                <button className={styles.text_btn}>U-4</button>
+                <button className={styles.text_btn}>U-5</button>
+                <button className={styles.text_btn}>U-6</button>
+              </p>
+              <p>
+                <button className={styles.text_btn}>U-10</button>
+                <button className={styles.text_btn}>U-12</button>
+              </p>
             </div>
           </div> 
-           
-       
         </div>
       </HeroSection>
 
       <HeroSection style={{background:'rgba(255, 229, 0, 0.7)'}} >
-        <div className="flex justify-center">
-          
-          <div className="m-10" style={{width:'400px'}}>
+        <div className={styles.home__card}>
+          <div className={styles.hidden_btn}>
+            <button className={styles.text_btn}>
+              <span className="m-2">
+                <i class="fas fa-chevron-right"></i>
+              </span>
+              MORE
+            </button>
+          </div>
+          <div className={styles.home__card_img}>
             <img src="./images/home-img-02.jpg"></img>
           </div>
-          <div className="m-10 mx-20">
-          <div className={styles.content_title}>
+          <div className={styles.home__card_content}>
+           <div className={styles.content_title}>
               <p className={styles.sub_title}>국가대표 선수와 함께하는</p>
-              <p><strong style={{color:'#ffe500'}}>BULLS</strong> ACADEMY</p>
+              <p><strong style={{color:'#fff'}}>BULLS</strong> ACADEMY</p>
             </div>
-            <div className={styles.academy}>
+            <div className={styles.text_btn_group }>
               <p>
                 <button className={styles.text_btn}>BEGINNER CLASS</button>
               </p>
@@ -84,7 +97,7 @@ export default function Home() {
             <p>GOYANG <strong style={{color:'#ffe500'}}>BULLS</strong> PLAY MOMENTS</p>
           </div>
           <div className="mb-10">
-            <iframe width="780" height="540" src="https://www.youtube.com/embed/fZ6CH94cNCs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe className={styles.home__video_content} src="https://www.youtube.com/embed/fZ6CH94cNCs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
         </div>
       </HeroSection>
@@ -94,9 +107,8 @@ export default function Home() {
           <div className={styles.content_title}>
             <p><strong style={{color:'#fff'}}>SNS</strong> on GOYANG BULLS</p>
           </div>
-          <div className="flex flex-row justify-center items-center">
-            
-              <ul className="flex flex-row justify-center items-center">
+          <div className={styles.home__sns_card_group}>
+              <ul className={styles.home__sns_card_group}>
                 {bullsInstagram.map((insta,i) => 
                   <li 
                     key={"bulls" + i}
@@ -104,8 +116,7 @@ export default function Home() {
                   >
                     <a href={insta.href}>
                       <img 
-                        className="rounded-xl shadow-xl"
-                        style={{width:"110px", height:"110px"}}
+                        className={styles.home__sns_card}
                         src={insta.img}
                       />
                     </a>
@@ -118,18 +129,17 @@ export default function Home() {
               <img src="/images/Instagram_Logo_Large.png" className="w-28" />
             </div>
             
-            <ul className="flex flex-row justify-center items-center">
+            <ul className={styles.home__sns_card_group}>
               {academyInstagram.map((insta,i) => 
                 <li 
                   key={"academy" + i}
                   className="m-2 "
                 >
                   <a href={insta.href}>
-                    <img 
-                      className="rounded-xl shadow-xl"
-                      style={{width:"120px", height:"120px"}}
-                      src={insta.img}
-                    />
+                  <img 
+                        className={styles.home__sns_card}
+                        src={insta.img}
+                      />
                   </a>
                 </li>
               )}
@@ -139,13 +149,12 @@ export default function Home() {
       </HeroSection>
 
       <HeroSection>
-        <div className="flex flex-row justify-between items-center w-full m-5">
-          <img style={{width:"10rem"}} src="/images/goyangsi_logo.jpg" />
-          <img style={{width:"10rem"}} src="/images/tikixaka_logo.png" />
-          <img style={{width:"10rem"}} src="/images/joma_logo.png" />
-          <img style={{width:"10rem"}} src="/images/goyangsoccer_logo.png" />
+        <div className={styles.home__sponsor_group}>
+          <img className={styles.home__sponsor_img} src="/images/goyangsi_logo.jpg" />
+          <img className={styles.home__sponsor_img} src="/images/tikixaka_logo.png" />
+          <img className={styles.home__sponsor_img} src="/images/joma_logo.png" />
+          <img className={styles.home__sponsor_img} src="/images/goyangsoccer_logo.png" />
         </div>
-
       </HeroSection>
 
 
