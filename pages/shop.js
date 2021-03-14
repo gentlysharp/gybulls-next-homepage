4e83e4ac26473124a287176d656b48ed6c075cf0
gentlysharp <sanghyuphan33@gmail.com>
2021-03-14 23:22:12 +0900

    comming soon message

--- pages/shop.js
+++ pages/shop.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Layout from '../components/core/Layout'
+import HeroSection from '../components/section/HeroSection'
+import home__styles from '../styles/Home.module.css'
 
 export default function shop() {
   return (
-    <div>
-      
-    </div>
+    <Layout>
+    <br></br>
+    <br></br>
+    <br></br>
+
+      <HeroSection>
+        <div className={home__styles.content_title} style={{borderBottom:"3px solid #181818"}}>
+          <p><strong style={{color:'#ffe500'}}>BULLS </strong>SHOP</p>
+        </div>
+      </HeroSection>
+
+      <HeroSection>
+        <div className={home__styles.content_title} >
+          <p>Comming Soon</p>
+        </div>
+      </HeroSection>
+    </Layout>
   )
 }
